3722bccc11635e1ae5a7b9ae492a4b256158c137
khaled759 <khalednabil759@gmail.com>
2026-03-22 17:13:55 +0200

    update scales after GELU fix

---
+++ Python_Modelling/HW_Optimization/HW_Model/Bert_Base/HW_test_samples.py
@@ -0,0 +1,7 @@
+from inferance import record_activations
+record_activations(
+    npz_path  = "D:\BERT_KHALED_SW\weights_with_scales.npz",
+    out_npz   = "D:\BERT_KHALED_SW\HW_TEST.npz",
+    out_index = "D:\BERT_KHALED_SW\HW_TEST.txt",
+    n_samples = 5,
+)
