ddf0fe2c0005e57a25afe2b7b9ba9505f375bde2
Mohamed Hussein <Mohamed_Hussein2100924@outlook.com>
2026-03-22 19:27:18 +0200

    PE module

---
+++ RTL/LayerNorm/PE_pkg.sv
@@ -0,0 +1,11 @@
+package PE_pkg;
+    // Opcodes defining the PE Mux routing
+    typedef enum logic [2:0] {
+        OP_LOAD_WGT   = 3'd0, // Load Gamma into local mem
+        OP_LOAD_BIAS  = 3'd1, // Load Beta into local mem
+        OP_PASS_X     = 3'd2, // Output X (For Mean Accumulation)
+        OP_VAR_SQR    = 3'd3, // Output (X - mu)^2
+        OP_NORMALIZE  = 3'd4, // Output (X - mu) * inv_sigma
+        OP_AFFINE     = 3'd5  // Output (X_norm * gamma) + beta
+    } pe_op_e;
+endpackage
